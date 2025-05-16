120ba0c2dbd191f57e051f876cf92e7782bedbe7
Harsh Raj <64323122+harshraj172@users.noreply.github.com>
2025-05-16 19:22:44 -0400

    add header

--- swebench/harness/utils.py
+++ swebench/harness/utils.py
@@ -20,6 +20,8 @@ from unidiff import PatchSet
 
 load_dotenv()
 
+HEADERS = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+
 
 class EvaluationError(Exception):
     def __init__(self, instance_id, message, logger):
@@ -305,7 +307,7 @@ def str2bool(v):
 def get_repo_file(repo, commit, filepath):
     url = f"https://raw.githubusercontent.com/{repo}/{commit}/{filepath}"
     try:
-        response = requests.get(url)
+        response = requests.get(url, headers=HEADERS)
         if response.status_code == 200:
             return response.text
         return None
